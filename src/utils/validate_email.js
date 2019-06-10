f47ed01ffa2e95bed1f2005d984a5fff932c91b8
Joé Jemmely <joe.jemmely@gmail.com>
2019-06-10 13:46:15 -0700

    jsdoc to validateEmail function

--- src/utils/validate_email.js
+++ src/utils/validate_email.js
@@ -1,1 +1,7 @@
-module.exports = email => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+/**
+ * Utility function to validate whether an email is valid
+ * @module validate_email
+ * @param {*} email
+ * @returns {boolean} is email valid
+ */
+module.exports = email => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.toString());
