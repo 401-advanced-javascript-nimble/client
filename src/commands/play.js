44a03b58379b4b5400199291989da1908103df93
Morgana Spake <morgana.spake@gmail.com>
2019-06-10 15:57:38 -0700

    game logic

--- src/commands/play.js
+++ src/commands/play.js
@@ -4,7 +4,7 @@ const commander = require('commander');
 const program = new commander.Command();
 
 program.command('play').action(() => {
-  console.log('Playing the game');
+  require('../lib/client.js');
 });
 
 program.parse(process.argv);
