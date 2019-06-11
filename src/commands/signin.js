8217187c2de1e97b0b15a89fa47722f547f02b94
Joé Jemmely <joe.jemmely@gmail.com>
2019-06-11 10:34:27 -0700

    base for signin

--- src/commands/signin.js
+++ src/commands/signin.js
@@ -1,7 +1,44 @@
 'use strict';
 
-function signIn() {
-  console.log('Signing in');
+require('dotenv').config();
+
+const superagent = require('superagent');
+const prompts = require('prompts');
+
+async function signIn() {
+  const questions = [
+    {
+      type: 'text',
+      name: 'username',
+      message: 'Enter your username',
+      validate: username => username != '',
+    },
+    {
+      type: 'password',
+      name: 'password',
+      message: 'Enter your password',
+    },
+  ];
+
+  try {
+    const { username, password } = await prompts(questions);
+
+    const response = await superagent
+      .post(`${process.env.API_SERVER_URI}/signin`)
+      .auth(username, password);
+    // console.log(response.res.text);
+
+    const { value: startNewGame } = await prompts({
+      type: 'confirm',
+      name: 'value',
+      message: 'Start a game?',
+      initial: true,
+    });
+
+    if (startNewGame) require('../lib/client.js');
+  } catch (error) {
+    console.error(JSON.parse(error.response.text).error);
+  }
 }
 
 module.exports = signIn;
