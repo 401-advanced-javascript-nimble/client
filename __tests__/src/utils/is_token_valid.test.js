41384c82546b3beef6a1f2c74606d0da20ff8167
Joé Jemmely <joe.jemmely@gmail.com>
2019-06-12 16:48:17 -0700

    Add tests for is_token_valid

--- __tests__/src/utils/is_token_valid.test.js
+++ __tests__/src/utils/is_token_valid.test.js
@@ -1,8 +1,24 @@
-// const supergoose = require('cf-supergoose');
+const Configstore = require('configstore');
+const packageJson = require('../../../package.json');
+const isTokenValid = require('../../../src/utils/is_token_valid.js');
+
 const User = require('../../../src/lib/User.js');
 
+const config = new Configstore(`${packageJson.name}`);
+
+const username = 'testuser';
+const password = 't3st!1234';
+
+const user = new User(username);
+
 describe('Is token valid', () => {
-  it('validate if a token is valid', () => {
-    const user = new User('test', 'password');
+  it('returns true when given a valid token', async () => {
+    await user.signIn(password);
+    expect(await isTokenValid()).toBeTruthy();
+  });
+
+  it('returns nothing when no token is found', async () => {
+    User.signOut();
+    expect(await isTokenValid()).toBeUndefined();
   });
 });
