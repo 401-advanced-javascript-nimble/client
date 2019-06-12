0bac6cbd3dd857095de1225d80a52c8a364a9487
Joé Jemmely <joe.jemmely@gmail.com>
2019-06-12 11:39:26 -0700

    Refactor menu

--- src/utils/menu.js
+++ src/utils/menu.js
@@ -1,15 +1,22 @@
+const prompts = require('prompts');
+const clear = require('clear');
+const figlet = require('figlet');
+
 const handlePlay = require('../commands/play.js');
 const handleLeaderboard = require('../commands/leaderboard.js');
 const handleSignOut = require('../commands/signout.js');
 
-module.exports = username => ({
-  type: 'select',
-  name: 'value',
-  message: `Welcome back ${username}!`,
-  choices: [
-    { title: '🎮  Play', value: handlePlay },
-    { title: '🏆  Leaderboard', value: handleLeaderboard },
-    { title: '👋  Signout', value: handleSignOut },
-  ],
-  initial: 0,
-});
+module.exports = async username => {
+  const fn = await prompts({
+    type: 'select',
+    name: 'value',
+    message: `Welcome back ${username}!`,
+    choices: [
+      { title: '🎮  Play', value: handlePlay },
+      { title: '🏆  Leaderboard', value: handleLeaderboard },
+      { title: '👋  Signout', value: handleSignOut },
+    ],
+    initial: 0,
+  });
+  if (typeof fn.value === 'function') fn.value();
+};
