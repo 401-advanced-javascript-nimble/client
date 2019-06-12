6a083f1426d7ec5d221bf1b415b8c9829c420a7a
Joé Jemmely <joe.jemmely@gmail.com>
2019-06-11 17:06:22 -0700

    Base for the welcome screen

---
+++ src/lib/welcome-screen.js
@@ -0,0 +1,81 @@
+const prompts = require('prompts');
+const figlet = require('figlet');
+const clear = require('clear');
+
+const handleSignUp = require('../commands/signup.js');
+const handleSignIn = require('../commands/signin.js');
+const handlePlay = require('../commands/play.js');
+const handleLeaderboard = require('../commands/leaderboard.js');
+const handleSignOut = require('../commands/signout.js');
+
+const Configstore = require('configstore');
+const packageJson = require('../../package.json');
+
+const config = new Configstore(packageJson.name);
+
+const newUser = [
+  {
+    type: 'toggle',
+    name: 'createAnAccount',
+    message: `Welcome stranger, let's start by creating an account`,
+    initial: true,
+    active: 'ok',
+    inactive: 'no',
+  },
+  {
+    type: 'text',
+    name: 'username',
+    message: `Alright! What's your username`,
+  },
+  {
+    type: 'text',
+    name: 'username',
+    message: `Great! What's your email (just in case you were to forget your password 🤷‍  )`,
+  },
+  {
+    type: 'text',
+    name: 'about',
+    message: 'Tell something about yourself',
+    initial: 'Why should I?',
+  },
+];
+
+const returningUser = {
+  type: 'select',
+  name: 'value',
+  message: 'What do you want to do?',
+  choices: [
+    {
+      title: '🔒  Create an new account',
+      value: handleSignUp,
+    },
+    {
+      title: '🔑  Sign in',
+      value: handleSignIn,
+      warn: 'Doh! You are already signed in',
+      disabled: true,
+    },
+    { title: '🎮  Play', value: handlePlay },
+    { title: '🏆  Leaderboard', value: handleLeaderboard },
+    { title: '👋  Signout', value: handleSignOut },
+  ],
+  initial: 2,
+};
+
+module.exports = async () => {
+  clear();
+  figlet('Nim', async (err, data) => {
+    if (err) {
+      console.log('Something went wrong...');
+      console.dir(err);
+      return;
+    }
+    console.log(data);
+
+    const fn = await prompts(
+      config.has('auth.token') ? returningUser : newUser
+    );
+
+    fn.value();
+  });
+};
