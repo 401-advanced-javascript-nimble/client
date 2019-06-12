4c2668787c85554bf8bed8ddbcfad15b808b1b05
Joé Jemmely <joe.jemmely@gmail.com>
2019-06-12 16:47:48 -0700

    Add tests for validate_password

--- __tests__/src/utils/validate_password.test.js
+++ __tests__/src/utils/validate_password.test.js
@@ -20,4 +20,14 @@ describe('Validate password', () => {
       expect(validatePassword('helloworld', 'strong')).toBeFalsy();
     });
   });
+
+  describe('Default', () => {
+    it('returns true when a good password is tested without specifying the strength', () => {
+      expect(validatePassword('helloWorld!19')).toBeTruthy();
+    });
+
+    it('returns false when a bad password is tested without specifying the strength', () => {
+      expect(validatePassword('helloworld', 'strong')).toBeFalsy();
+    });
+  });
 });
